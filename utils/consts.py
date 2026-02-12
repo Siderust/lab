f9d02c800f9a198ebf00559b9455c76992da6e03
ramon.valles <ramon.valles-puig@cta-consortium.org>
2026-02-12 18:20:07 +0100

    cleanup

--- utils/consts.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Definir rutas a los datasets
-lab_root = os.environ["SIDERUST_LAB_ROOT"]
-astropy_path = os.path.join(lab_root, "astropy/dataset/")
-libnova_path = os.path.join(lab_root, "libnova/dataset/")
-siderust_path = os.path.join(lab_root, "siderust/dataset/")
-planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
